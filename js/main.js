f312fe189f38ac7708115629b3906d03a2ca8d2b
JoseArroyave <jarroyave@unal.edu.cogit config --global user.email jarroyave@unal.edu.co>
2022-04-11 00:35:56 -0500

    scrollspy y commit final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Sweet alert del boton suscribirse
+
 // Con jQuery
 $('#btnSuscribirse').click(function (e) {
   e.preventDefault()
@@ -9,13 +11,32 @@ $('#btnSuscribirse').click(function (e) {
 })
 
 // JavaScript puro
+var btnSuscribirse = document.getElementById('btnSuscribirse')
+btnSuscribirse.addEventListener('click', function (e) {
+  e.preventDefault()
+  Swal.fire(
+    'Excelente!',
+    'Te has suscrito a nuestro newsletter.',
+    'success'
+  )
+})
 
-// var btnSuscribirse = document.getElementById('btnSuscribirse')
-// btnSuscribirse.addEventListener('click', function (e) {
-//   e.preventDefault()
-//   Swal.fire(
-//     'Excelente!',
-//     'Te has suscrito a nuestro newsletter.',
-//     'success'
-//   )
-// })
+// Iniciar Scrollspy
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#menu-navegacion'
+})
+
+// Scroll suavizado
+scrollSpy.addEventListener('click', function (event) {
+  if (this.hash != '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+      window.location.hash = hash;
+    })
+  }
+})
